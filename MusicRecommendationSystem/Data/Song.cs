d8389adfebbb6ceb18fef3053b55aacae0ffacc5
Mark Grandell <91348195+mjgrandell@users.noreply.github.com>
2023-03-21 21:34:06 -0400

    Add files via upload

---
+++ MusicRecommendationSystem/Data/Song.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicRecommendationSystem.Data
+{
+    public class Song
+    {
+        public string SongTitle { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Genre { get; set; }
+        public int Cluster { get; set; }
+        public string SongAndArtist { get; set; }
+    }
+}
--- MusicRecommendationSystem/Program.cs
+++ MusicRecommendationSystem/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System.Data;
+using MusicRecommendationSystem.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<SongService>();
 
 var app = builder.Build();
 
